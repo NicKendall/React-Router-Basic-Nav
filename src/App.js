38532c63278fa3b783cea028ef226893c2a3194f
nicbongo <nicbongo@protonmail.com>
2019-12-16 16:54:44 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { Home, About, Contact, Navigation } from './components';
+import './App.css';
+
 
 const App = () => (
+  
   <div>
     <Navigation />
+    
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/contact">
+      <Contact />
+    </Route>
   </div>
+ 
 );
 
 export default App;
+
